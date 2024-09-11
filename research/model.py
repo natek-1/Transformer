5bfad1c0ee06fef433eab7572ea13ba321db8ac6
Nathan Gabriel <nategabrielk@icloud.com>
2024-09-11 19:58:26 -0400

    added position encodign

--- research/model.py
+++ research/model.py
@@ -16,3 +16,31 @@ class InputEmbeddings(nn.Module):
         return self.embedding(x) * math.sqrt(self.d_model)
 
 
+class PositionEncoding(nn.Module):
+
+    def __init__(self, d_model: int, seq_len: int, dropout: float):
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+
+        position_encoding = torch.zeros(self.seq_len, self.d_model) # (seq_len, d_model)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1) #(seq_len, 1)
+        div_term = torch.exp(torch.arange(0, self.d_model, 2).float() * (-math.log(10000.0) /  self.d_model)) # (d_model/2)
+
+        position_encoding[:,0::2] = torch.sin(position * div_term) # sin to even position
+        position_encoding[:,1::2] = torch.cos(position * div_term) # cos to odd position
+
+        # add dimention for batch_size
+        position_encoding = position_encoding.unsqueeze(0)  # (1, seq_len, d_model)
+        self.register_buffer("pe", position_encoding)
+
+    def forward(self, x: torch.Tensor):
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False) # (batch_size, seq_len, d_model)
+        return self.dropout(x)
+    
+
+
+
+
