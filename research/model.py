ebf7115e014f482bda6db0352a95aab0c6176695
Nathan Gabriel <nategabrielk@icloud.com>
2024-09-14 12:29:19 -0400

    done decoder block

--- research/model.py
+++ research/model.py
@@ -155,3 +155,20 @@ class Encoder(nn.Module):
             x = layer(x, mask)
         return self.norm(x)
     
+
+class DecoderBlock(nn.Module):
+
+    def __init__(self, features: int, self_attention_block: MultiheadAttentionBlock,
+                 cross_attention_block: MultiheadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float):
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.cross_attention_block = cross_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connection = nn.ModuleList([ResidualConnection(features, dropout) for _ in range(3)])
+    
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        x = self.residual_connection[0](x, lambda x: self.self_attention_block(x, x,x, tgt_mask))
+        x = self.residual_connection[1](x, lambda x: self.cross_attention_block(x, encoder_output, encoder_output, src_mask))
+        x = self.residual_connection[2](x, self.feed_forward_block)
+        return x
+    
