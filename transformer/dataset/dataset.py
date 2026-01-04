39825c5665b9db7505a218f84957acf067bc483d
Nathan Gabriel <nategabrielk@icloud.com>
2026-01-03 23:04:13 -0500

    added dataset file and updated config

---
+++ transformer/dataset/dataset.py
@@ -0,0 +1,77 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+
+
+class BilingualDataset(Dataset):
+
+    def __init__(self, ds, src_tokenizer, tgt_tokenizer, src_lang, tgt_lang, seq_len):
+        super().__init__()
+        self.seq_len = seq_len
+
+        self.ds = ds
+        self.src_tokenizer = src_tokenizer
+        self.tgt_tokenizer = tgt_tokenizer
+        self.src_lang = src_lang
+        self.tgt_lang = tgt_lang
+
+        self.sos_token = torch.tensor([tgt_tokenizer.token_to_id("[SOS]")], dtype=torch.int64)
+        self.eos_token = torch.tensor([tgt_tokenizer.token_to_id("[EOS]")], dtype=torch.int64)
+        self.pad_token = torch.tensor([tgt_tokenizer.token_to_id("[PAD]")], dtype=torch.int64)
+
+    def __len__(self):
+        return len(self.ds)
+
+    def __getitem__(self, index):
+        src_target_pair = self.ds[index]
+        src_text = src_target_pair['translation'][self.src_lang]
+        tgt_text = src_target_pair['translation'][self.tgt_lang]
+
+        enc_input_token = self.src_tokenizer.encode(src_text).ids
+        dec_input_token = self.tgt_tokenizer.encode(tgt_text).ids
+
+        enc_num_padding_tokens = self.seq_len - len(enc_input_token) - 2 # we will add <SOS> and <EOS> token to encoding or input
+        dec_num_padding_tokens = self.seq_len - len(dec_input_token) - 1 # we will add <SOS> xor <EOS> token to input decoding for training and label repectively
+
+        if enc_num_padding_tokens < 0 or dec_num_padding_tokens < 0:
+            raise ValueError("Sentence is too long")
+
+        encoder_input = torch.cat([
+            self.sos_token,
+            torch.tensor(enc_input_token, dtype=torch.int64),
+            self.eos_token,
+            torch.tensor([self.pad_token] * enc_num_padding_tokens, dtype=torch.int64)
+        ], dim=0) # expected model input
+
+        decoder_input = torch.cat([
+            self.sos_token,
+            torch.tensor(dec_input_token, dtype=torch.int64),
+            torch.tensor([self.pad_token] * dec_num_padding_tokens, dtype=torch.int64)
+        ], dim=0) # input to decoder
+
+        label = torch.cat([
+            torch.tensor(dec_input_token, dtype=torch.int64),
+            self.eos_token,
+            torch.tensor([self.pad_token] * dec_num_padding_tokens, dtype=torch.int64)
+        ], dim=0) #output from decoder (model output) used during training
+
+        # asinity check
+        assert encoder_input.size(0) == self.seq_len
+        assert decoder_input.size(0) == self.seq_len
+        assert label.size(0) == self.seq_len
+
+        return {
+            "encoder_input": encoder_input, # (seq_len)
+            "decoder_input": decoder_input, # (seq_len)
+            "encoder_mask": (encoder_input != self.pad_token).unsqueeze(0).unsqueeze(0).int(), # (1, 1, seq_len)
+            "decoder_mask": (decoder_input != self.pad_token).unsqueeze(0).unsqueeze(0).int() & BilingualDataset.casual_mask(decoder_input.size(0)), # (1, 1, seq_len) & #(1, seq_len, seq_len)
+            "label": label,
+            "src_text": src_text,
+            "tgt_text": tgt_text
+
+        }
+    
+    @staticmethod
+    def casual_mask(size):
+        mask = torch.triu(torch.ones((1, size, size)), diagonal=1).type(torch.int64)
+        return mask == 0
